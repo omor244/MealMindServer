490c8c6e08995833c298cb5dbd8f9e58873811e6
Abdur55 <mrrahim972@gmail.com>
2026-01-28 14:14:37 +0600

    add user delete api

--- index.js
+++ index.js
@@ -64,6 +64,17 @@ async function run() {
 
 
         })
+        app.delete('/users/:id', async (req, res) => {
+        
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+          
+            const result = await usercoll.deleteOne(query)
+
+            res.send(result)
+
+
+        })
 
         app.post("/recipes", async (req, res) => {
 
